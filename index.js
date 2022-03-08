23fdb05f0d061642d9da54f63e8f11a8d855b0a1
Christopher Perez <ChrisAmadeusPerez@gmail.com>
2022-03-08 15:40:04 -0500

    commit tree 2e7f978673c7daaad7496cc584993bf06ea6db9a

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    let upperString = string.charAt(0).toUpperCase() + string.slice(1)
+    return upperString
+  }
+
+  static sanitize(string){
+    let filteredString = string.replace(/[^a-z0'-\s\.,]/gim, '')
+    return filteredString
+  }
+
+  static titleize(string){
+    let exemptWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let stringArray = string.toLowerCase().split(" ")
+
+    let stringArrayCapitalize = stringArray.map(function(s){
+      for(let i = 0; i < exemptWords.length; i++){
+        if(exemptWords[i] === s){
+          return s
+        }
+      }
+      return s.charAt(0).toUpperCase() + s.slice(1)
+    }).join(' ')
+
+    stringArrayCapitalize = stringArrayCapitalize.charAt(0).toUpperCase() + stringArrayCapitalize.slice(1)
+
+    return stringArrayCapitalize
+  }
 }
